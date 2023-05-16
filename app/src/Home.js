356dc34ab0e4fa72e6c0aaf517dace471f987342
Angel Chen <angel666@g.ucla.edu>
2023-05-16 16:44:05 -0700

    Home, Login and Register page initiated

---
+++ app/src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return ( 
+        <div>
+            <h1>Welcome to MatchU</h1>
+        </div>
+     );
+}
+ 
+export default Home;
---
+++ app/src/Register.js
@@ -0,0 +1,9 @@
+const Register = () => {
+    return (
+        <div>
+            <h1>User Registeration</h1>
+        </div>
+      );
+}
+ 
+export default Register;
